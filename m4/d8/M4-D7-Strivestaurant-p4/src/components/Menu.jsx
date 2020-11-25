469effef8a275f543864faba7c5683b633e39162
elinoza <hillcakmak@gmail.com>
2020-11-25 19:25:58 +0300

    dd

--- m4/d8/M4-D7-Strivestaurant-p4/src/components/Menu.jsx
+++ m4/d8/M4-D7-Strivestaurant-p4/src/components/Menu.jsx
@@ -6,6 +6,7 @@ import { Container } from "react-bootstrap";
 const Menu = () => (
   <Container>
     {dishes && dishes.map((dish) => <SingleDish dish={dish} key={dish.id} />)}
+    {!dishes && <h1> WORK IN PROGRESS</h1>}
   </Container>
 );
 export default Menu;
