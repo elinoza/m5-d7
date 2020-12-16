79fef8c595a3b5d368fc9ea09341998622108115
elinoza <hillcakmak@gmail.com>
2020-12-16 16:56:29 +0300

    hello3

--- src/server.js
+++ src/server.js
@@ -16,24 +16,24 @@ const port = process.env.PORT || 3001 // the fallback is for local development, 
 
 server.use(express.json())
 
-// const whiteList =
-//   process.env.NODE_ENV === "production"
-//     ? [process.env.FE_URL_PROD]
-//     : [process.env.FE_URL_DEV]
+const whiteList =
+  process.env.NODE_ENV === "production"
+    ? [process.env.FE_URL_PROD]
+    : [process.env.FE_URL_DEV]
 
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whiteList.indexOf(origin) !== -1) {
-//       // allowed
-//       callback(null, true)
-//     } else {
-//       // Not allowed
-//       callback(new Error("NOT ALLOWED - CORS ISSUES"))
-//     }
-//   },
-// }
-// server.use(cors(corsOptions)) // CROSS ORIGIN RESOURCE SHARING
-server.use(cors())
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whiteList.indexOf(origin) !== -1) {
+      // allowed
+      callback(null, true)
+    } else {
+      // Not allowed
+      callback(new Error("NOT ALLOWED - CORS ISSUES"))
+    }
+  },
+}
+server.use(cors(corsOptions)) // CROSS ORIGIN RESOURCE SHARING
+// server.use(cors())
 //ROUTES
 
 server.use("/books", booksRoutes)
